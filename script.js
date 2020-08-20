624663a7aad68e97498fd65574b63da602912f46
Nidhi-gharat-24 <69667751+Nidhi-gharat-24@users.noreply.github.com>
2020-08-20 21:41:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+(function () {
+
+var names = ["prit", "John", "linda", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+  
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
